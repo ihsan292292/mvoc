5dcdd59f923cacbcce45409c28b2c34c2a6f0c5d
ihsan292292 <97184876+ihsan292292@users.noreply.github.com>
2022-01-07 09:41:25 +0530

    initial

---
+++ pythonProject/define three sets.py
@@ -0,0 +1,11 @@
+
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
+
+print("Union of E and N is :",E.union(N))
+
+print("Intersection of E and N is",E.intersection(N))
+
+print("Difference of E and N is",E.difference(N))
+
+print("Symmetric difference of E and N is",E.symmetric_difference(N))
---
+++ pythonProject/sort words.py
@@ -0,0 +1,10 @@
+
+my_str = "Hello this Is an Example with cased letters"
+
+words = my_str.split()
+
+words.sort()
+
+print("The sorted words are: ")
+for word in words:
+    print(word)
