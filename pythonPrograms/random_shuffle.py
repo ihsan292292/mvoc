b6dffd709153938ab76510b3eeb62dd9022776e7
ihsan292292 <97184876+ihsan292292@users.noreply.github.com>
2022-01-08 10:07:08 +0530

    Create random_shuffle.py

---
+++ pythonPrograms/random_shuffle.py
@@ -0,0 +1,9 @@
+import itertools, random
+
+deck = list(itertools.product(range(1,14),['spade','heart','diamond','club']))
+
+random.shuffle(deck)
+
+print("you got: ")
+for i in range(5):
+    print(deck[i][0],"of",deck[i][1])
