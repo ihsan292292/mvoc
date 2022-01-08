134e3a158a057208a5895cc1b3bcf8284f4fc4e8
ihsan292292 <97184876+ihsan292292@users.noreply.github.com>
2022-01-08 09:01:07 +0530

    calculator changed

---
+++ phpPrograms/calculator.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+    <?php
+    $a =12;
+    $b =13;
+    echo "<br><br><br><br><br><h1><span style='color:#00008B'<b><center>CALCULATOR</h1>"; 
+    echo "<center><h3><b>First No: ",$a,"<br></h3>";
+    echo "<h3>Second No: ",$b, "<br></h3>";
+    echo "<h2>Sum :",$a+$b,"<br></h2>";
+    echo "<h2>Subtraction :",$a-$b,"<br></h2>";
+    echo "<h2>Multiplication :",$a*$b,"<br></h2>";
+    echo "<h2>Division :",$a/$b,"<br></h2>";
+    echo '<body style="background-color:#FFF8DC">'
+?>
+    </body>
+</html>
